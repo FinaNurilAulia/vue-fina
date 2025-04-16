33752d1c91cb0db8f8ef6b26ef15e21c34f63b3e
FinaNurilAulia <FinaNurilAulia>
2025-04-16 11:47:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { getUsers } from "./services/api";
+
+const users = ref([]);
+
+onMounted(async () => {
+  users.value = await getUsers();
+});
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="mahasiswa in users.results" :key="user.id">{{ user.nama }}</li>
+    </ul>
+  </div>
+</template>
---
+++ src/worker.js
@@ -0,0 +1,15 @@
+export default {
+    async fetch(request, env) {
+      const { pathname } = new URL(request.url);
+  
+      if (pathname === "/api/mahasiswa") {
+        // Ambil data dari D1
+        const result = await env.DB.prepare("SELECT * FROM mahasiwa").all();
+        return new Response(JSON.stringify(result), {
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+  
+      return new Response("Hello, World!");
+    },
+  };
